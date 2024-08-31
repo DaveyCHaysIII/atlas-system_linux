3f7166f9eb33a7467201e072f32076602d1e849b
Davey <david.c.hays.iii@gmail.com>
2024-08-31 21:16:06 +0000

    stopping work for now

---
+++ getline/_getline.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "_getline.h"
+
+/**
+ * _getline- get a line from a file descriptor
+ * @fd: the file in question
+ *
+ * Return: null-terminated string that does not include newline
+ */
+
+char *_getline(const int fd)
+{
+
+}
---
+++ getline/_getline.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define READ_SIZE 1024
+
+char *_getline(const int fd)
+
+#endif

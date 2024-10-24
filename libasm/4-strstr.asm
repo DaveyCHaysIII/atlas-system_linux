b74adf524b8b19aa2a43a4d3844f40ce77a50291
Davey C Hays III <david.c.hays.iii@gmail.com>
2024-10-24 04:02:11 +0000

    added strstr

---
+++ libasm/4-strstr.asm
@@ -0,0 +1,45 @@
+BITS 64
+
+section .text
+	global asm_strstr
+
+asm_strstr:
+	test rdi, rdi
+	jz .null_done
+	test rsi, rsi
+	js .null_done
+
+.main_loop:
+	mov al, byte [rdi]
+	mov bl, byte [rsi]
+	cmp al, 0
+	je .null_done
+	cmp al, bl
+	jne .next_char
+	xor r10, r10
+
+.inner_loop:
+	mov al, byte [rdi + r10]
+	mov bl, byte [rsi + r10]
+	cmp bl, 0
+	je .found
+	cmp al, 0
+	je .null_done
+
+	cmp al, bl
+	jne .next_char
+	inc r10
+	jmp .inner_loop
+
+.next_char:
+	inc rdi
+	jmp .main_loop
+
+.null_done:
+	xor rax, rax
+	ret
+
+.found:
+	mov rax, rdi
+	ret
+

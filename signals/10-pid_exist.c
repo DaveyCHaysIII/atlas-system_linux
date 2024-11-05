6a552d921285747dcc066a8f4059cf515862763a
Davey C Hays III <david.c.hays.iii@gmail.com>
2024-11-05 04:24:55 +0000

    added pid_exist

---
+++ signals/10-pid_exist.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <signal.h>
+
+/**
+ * pid_exit - checks if a pid exists
+ * @pid: the pid in question
+ *
+ * Return: pid or 0
+ */
+
+int pid_exist(pid_t pid)
+{
+	return (kill(pid, 0) == 0);
+}
+

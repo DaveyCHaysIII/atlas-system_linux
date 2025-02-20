1baa32211453dcbf6832378744d38eec85995769
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2025-02-20 12:01:28 -0600

    fixed port problem

--- sockets/0-server.c
+++ sockets/0-server.c
@@ -8,10 +8,15 @@
 int main()
 {
 	int sock_fd, bind_fd;
-	uint32_t port;
-	struct sockaddr_in serv_addr;
+	struct sockaddr_in serv_addr = {
+		AF_INET,
+		htons(12345),
+		{
+			htons(INADDR_ANY)
+		},
+		{0x00}
+	};
 
-	port = 12345;
 	sock_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sock_fd < 0)
 	{
@@ -19,9 +24,6 @@ int main()
 		exit(EXIT_FAILURE);
 	}
 
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	serv_addr.sin_port = htonl(12345);
 
 	bind_fd = bind(sock_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
 	if (bind_fd < 0)
@@ -29,10 +31,12 @@ int main()
 		perror("bind");
 		exit(EXIT_FAILURE);
 	}
-	printf("Server listening on port %d\n", port);
+	listen(sock_fd, 10);
+	printf("Server listening on port 12345\n");
+
 	while (1)
 	{
-		listen(sock_fd, 10);
+
 	}
 	return (0);
 }

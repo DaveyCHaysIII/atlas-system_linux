aca85ff19f3de9a241196a15fcfe4a72474b36af
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2025-02-20 14:07:52 -0600

    fixed betty for CLANG documentation

--- sockets/0-server.c
+++ sockets/0-server.c
@@ -8,14 +8,12 @@
 int main(void)
 {
 	int sock_fd, bind_fd;
-	struct sockaddr_in serv_addr = {
-		AF_INET,
-		htons(12345),
-		{
-			htons(INADDR_ANY)
-		},
-		{0x00}
-	};
+	struct sockaddr_in serv_addr;
+
+	memset(&serv_addr, 0x00, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(12345);
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
 	sock_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sock_fd < 0)
--- sockets/sockets.h
+++ sockets/sockets.h
@@ -3,9 +3,14 @@
 
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/un.h>
+#include <netdb.h>
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
 
 #endif

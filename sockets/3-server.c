7af991378658fdaa776200ada89753799fdc8808
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2025-02-21 21:18:38 -0600

    added 3-server.c

---
+++ sockets/3-server.c
@@ -0,0 +1,51 @@
+#include "sockets.h"
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int sock_fd, bind_fd, s;
+	struct sockaddr_in serv_addr;
+	socklen_t addr_len = sizeof(serv_addr);
+	char recvbuf[50];
+	ssize_t bytes_recieved;
+
+	memset(&serv_addr, 0x00, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(12345);
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock_fd < 0)
+	{
+		perror("socket");
+		exit(EXIT_FAILURE);
+	}
+	bind_fd = bind(sock_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+	if (bind_fd < 0)
+	{
+		perror("bind");
+		exit(EXIT_FAILURE);
+	}
+	if (listen(sock_fd, 10) < 0)
+	{
+		perror("listen");
+		exit(EXIT_FAILURE);
+	}
+	printf("Server listening on port 12345\n");
+	s = accept(sock_fd, (struct sockaddr *)&serv_addr, &addr_len);
+	if (s < 0)
+	{
+		perror("accept");
+		exit(EXIT_FAILURE);
+	}
+	printf("Client Connected: %s\n", inet_ntoa(serv_addr.sin_addr));
+	bytes_recieved = recv(s, recvbuf, sizeof(recvbuf) - 1, 0);
+	recvbuf[bytes_recieved] = '\0';
+	printf("Message Recieved: %s\n", recvbuf);
+	return (0);
+}
+
+

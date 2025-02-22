70a08e00efd01db87a38257ead1d53f555e9e2eb
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2025-02-22 13:15:27 -0600

    adding RESTAPI - request

--- sockets/sockets.h
+++ sockets/sockets.h
@@ -1,6 +1,8 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+#define MAX_BUFF 1024
+
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -12,8 +14,28 @@
 #include <string.h>
 #include <unistd.h>
 
+/**
+ * httpreq - struct to hold parsed HTTP data
+ * @method: HTTP method (GET, POST, etc)
+ * @path: the path its looking for
+ * @version: the HTTP version
+ * @host: the host (localhost, etc)
+ * @user_agent: I don't know this one
+ * @accept: the accept
+ */
+
+typedef struct {
+	char method[8];
+	char path[MAX_BUFF];
+	char version[16];
+	char host[MAX_BUFF];
+	char user_agent[MAX_BUFF];
+	char accept[MAX_BUFF];
+} httpreq;
 
 int create_ssocket(int port, int backlog);
 int create_csocket(const char *ip, int port);
-
+int handle_accept(int sock_fd, struct sockaddr_in *addr, socklen_t *addr_len);
+int parse_req(const char *recvbuf, httpreq *req);
+void print_req(httpreq *req);
 #endif
--- sockets/utils.c
+++ sockets/utils.c
@@ -13,9 +13,9 @@
 
 int create_ssocket(int port, int backlog)
 {
-	int sock_fd
+	int sock_fd, bind_fd, optval = 1;
 	struct sockaddr_in serv_addr;
-	ssize_t addr_len;
+	ssize_t addr_len, op_len;
 
 	sock_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sock_fd < 0)
@@ -23,14 +23,18 @@ int create_ssocket(int port, int backlog)
 		perror("serv_socket");
 		exit(EXIT_FAILURE);
 	}
-
+	op_len = sizeof(optval);
+	if (setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &optval, op_len) < 0)
+	{
+		perror("serv_sockopt");
+		close(sock_fd);
+		exit(EXIT_FAILURE);
+	}
 	memset(&serv_addr, 0x00, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_port = htons(port);
 	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-
 	addr_len = sizeof(serv_addr);
-
 	bind_fd = bind(sock_fd, (struct sockaddr *)&serv_addr, addr_len);
 	if (bind_fd < 0)
 	{
@@ -55,7 +59,7 @@ int create_ssocket(int port, int backlog)
  * @port: the port to use
  *
  * This function handles the socket() initialization for
- * a client socket
+ * a client socket (**unused in this project**)
  *
  * Return: the socket FD
  */
@@ -66,6 +70,13 @@ int create_csocket(const char *ip, int port)
 	struct sockaddr_in serv_addr;
 	ssize_t addr_len;
 
+	sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock_fd < 0)
+	{
+		perror("client_socket");
+		exit(EXIT_FAILURE);
+	}
+
 	memset(&serv_addr, 0x00, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_port = htons(port);
@@ -73,9 +84,9 @@ int create_csocket(const char *ip, int port)
 
 	addr_len = sizeof(serv_addr);
 
-	if (connect(sock_fd, (stuct sockaddr *)&serv_addr, addr_len) < 0)
+	if (connect(sock_fd, (struct sockaddr *)&serv_addr, addr_len) < 0)
 	{
-		perror(client_connect);
+		perror("client_connect");
 		close(sock_fd);
 		exit(EXIT_FAILURE);
 	}
@@ -83,16 +94,66 @@ int create_csocket(const char *ip, int port)
 }
 
 /**
+ * handle_accept - handles the accept function
+ * @sock_fd: the socket fd for the server
+ * @addr: the sockaddr_in struct for the client
+ * @addr_len: the length of the addr struct
  *
+ * Return: the client fd (exit failure on err)
  */
 
+int handle_accept(int sock_fd, struct sockaddr_in *addr, socklen_t *addr_len)
+{
+	int client_fd;
+
+	client_fd = accept(sock_fd,
+		(struct sockaddr *)addr,
+		addr_len);
+	if (client_fd < 0)
+	{
+		perror("accept");
+		close(sock_fd);
+		exit(EXIT_FAILURE);
+	}
+	printf("Client connected: %s\n", inet_ntoa(addr->sin_addr));
+	return (client_fd);
+}
+
+/**
+ * parse_req - parse an HTTP request
+ * @recvbuf: raw req string
+ * @req: pointer to data structure to hold parsed data
+ *
+ * Return: 0 on success
+ */
+
+int parse_req(const char *recvbuf, httpreq *req)
+{
+	if (sscanf(recvbuf,
+		"%s %s %s",
+		req->method,
+		req->path,
+		req->version) < 3)
+
+	{
+		perror("parse");
+		exit(EXIT_FAILURE);
+	}
+	return (0);
+}
 
 
 /**
+ * print_req - print an HTTP request
+ * @req: the data structure to pull from
  *
+ * Return: no return
  */
 
-
-/**
- *
- */
+void print_req(httpreq *req)
+{
+	printf("Method: %s\nPath: %s\nVersion: %s\n",
+			req->method,
+			req->path,
+			req->version);
+}
